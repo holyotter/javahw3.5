d429d602572b0db2e5166b1a24959c3fe858d851
Vladislav Semenov <arctic.ratatat@gmail.com>
2022-01-13 00:02:55 +0300

    third commit

--- src/test/java/ru/netology/manager/ProductManagerTest.java
+++ src/test/java/ru/netology/manager/ProductManagerTest.java
@@ -15,6 +15,7 @@ class ProductManagerTest {
 
     private Product firstBook = new Book(1, "Good Omens", 700, "Pratchett Terry");
     private  Product secondBook = new Book(2, "The Joy of JavaScript", 900, "Luis Atencio");
+    private Product thirdBook = new Book(3, "The Omen", 800, "David Seltzer");
     private Product firstSmartphone = new Smartphone(3, "Google Pixel 5 8/128Gb Black", 60000, "HTC Corporation");
     private  Product secondSmartphone = new Smartphone(4, "Смартфон Xiaomi Mi 11 256GB Horizon Blue", 62000, "Xiaomi Corporation");
 
@@ -22,13 +23,13 @@ class ProductManagerTest {
     public void setUp(){
         repository.save(firstBook);
         repository.save(secondBook);
+        repository.save(thirdBook);
         repository.save(firstSmartphone);
         repository.save(secondSmartphone);
     }
 
     @Test // позитивный сценарий/поиск книги по названию
     public void findBookWithName() {
-        manager.matches(firstBook, "Good Omens");
 
         Product[] expected = new Product[]{firstBook};
         Product[] actual = manager.searchBy("Good Omens");
@@ -38,7 +39,6 @@ class ProductManagerTest {
 
     @Test // позитивный сценарий/поиск книги по автору
     public void findBookWithAuthor() {
-        manager.matches(secondBook, "Pratchett Terry");
 
         Product[] expected = new Product[]{firstBook};
         Product[] actual = manager.searchBy("Pratchett Terry");
@@ -91,6 +91,15 @@ class ProductManagerTest {
     public void thereIsNoSmartphoneWhithSuchManufacturerInRepo() {
         Product[] expected = new Product[0];
         Product[] actual = manager.searchBy("Huawei");
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void findSomeBooksWithTheSameName() {
+
+        Product[] expected = new Product[]{firstBook, thirdBook};
+        Product[] actual = manager.searchBy("Omen");
 
         assertArrayEquals(expected, actual);
     }
